e08a9d47072bdce3eafe2f1f8c0b77a29632b36c
Sean McGowan <spmcgowan@gmail.com>
2019-07-28 22:10:47 -0400

    Allow cancelations to requests

---
+++ context/context.go
@@ -0,0 +1,31 @@
+package context1
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// Server is a server
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		data := make(chan string, 1)
+
+		go func() {
+			data <- store.Fetch()
+		}()
+
+		select {
+		case d := <-data:
+			fmt.Fprint(w, d)
+		case <-ctx.Done():
+			store.Cancel()
+		}
+	}
+}
+
+// Store interface
+type Store interface {
+	Fetch() string
+	Cancel()
+}
