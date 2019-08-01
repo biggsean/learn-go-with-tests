9226a012bc67431a352c3a502941b446f571fa70
Sean McGowan <spmcgowan@gmail.com>
2019-08-01 10:39:35 -0400

    Update context

--- context/context.go
+++ context/context.go
@@ -1,31 +1,18 @@
 package context1
 
 import (
-	"fmt"
+	"context"
 	"net/http"
 )
 
 // Server is a server
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		data := make(chan string, 1)
-
-		go func() {
-			data <- store.Fetch()
-		}()
-
-		select {
-		case d := <-data:
-			fmt.Fprint(w, d)
-		case <-ctx.Done():
-			store.Cancel()
-		}
+		//pass
 	}
 }
 
 // Store interface
 type Store interface {
-	Fetch() string
-	Cancel()
+	Fetch(ctx context.Context) (string, error)
 }
